d007050df91f091adb5c29348b79459ea6a739d9
manhunter7-2 <thomas.piquet7@gmail.com>
2022-05-16 12:01:40 +0200

    16/05 Dropdown Bootstrap + new page

--- Templates/header.php
+++ Templates/header.php
@@ -1,11 +1,12 @@
 <header>
     <div>Test header</div>
-    <form method="post" class="goToForm">
-        <label>
-            <select>
-                <option value="Jesaispas">Je sais pas</option>
-                <option value="Onverra">On verra</option>
-            </select>
-        </label>
-    </form>
+    <div class="btn-group dropleft" id="header-dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Menus
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="<?php echo($GLOBALS['PAGES']."creators_profile.php"); ?>">Creator's profile</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
 </header>
