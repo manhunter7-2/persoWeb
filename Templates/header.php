aff2e5b67b09ed77c3331da0f011ef52a582c3f0
Manhunter7 <thomas.piquet7@gmail.com>
2022-05-22 21:29:32 +0200

    Meme page creation

--- Templates/header.php
+++ Templates/header.php
@@ -6,7 +6,8 @@
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="<?php echo($GLOBALS['PAGES']."creators_profile.php"); ?>">Creator's profile</a>
-    <a class="dropdown-item" href="">Something else here</a>
+    <a class="dropdown-item" href="<?php echo($GLOBALS['PAGES']."meme_square.php"); ?>">Memes square</a>
+    <a class="dropdown-item" href=<?php echo($GLOBALS ['TOOLS']."tests.php"); ?>">Tests (WARNING) </a>
   </div>
 </div>
 </header>
